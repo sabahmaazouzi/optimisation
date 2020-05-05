594fc07d7a78a0af3c61b66b89ec922586734445
sabahmaazouzi <59852545+sabahmaazouzi@users.noreply.github.com>
2020-05-05 02:56:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
+
+
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
-int len=2;
+int len=2,k=0;
 vector <vector<float>> h={{1,0},{0,1}};
-float alpha=1;
-vector<float> d={-1,1};
-vector<float> x={0,0},b={0,0},a={-1,1};
-vector <vector<float>> d_d={{0,0},{0,0}};
+float alpha;
+vector<float> d;
+vector<float> x={0,0};
 
 float fonction(vector<float> x)
 {
@@ -18,7 +20,7 @@ float fonction(vector<float> x)
     
 }
 vector<float> gradientf(vector<float> x) //graduient
-{vector<float> f(2,0);
+{vector<float> f(len,0);
     for(int i=0;i<len;i++)
         { if (i==0)
             f[i]=(1+4*(x[0])+2*(x[1]));
@@ -48,7 +50,7 @@ vector<float> y(vector<float>a ,vector<float>b )
         vector<float>y;
         vector<float> gradienta=gradientf(a),gradientb=gradientf(b);
         for(int i=0;i<len;i++)
-            y.push_back(gradienta[i]-gradientb[i]);
+            y.push_back(gradientb[i]-gradienta[i]);
         return y;
         
     }
@@ -75,14 +77,26 @@ vector<float> produitmv(vector <vector<float>> h,vector<float> y)
     return p;
     
 }
-
+vector<float> alphaxvect(int a , vector<float> m)
+{
+    for(int i=0;i<len;i++)
+        m[i]=m[i]*a;
+    return m;
+    
+}
+vector<float> somme(vector<float> a,vector<float>b)
+{  vector<float> s={0,0};
+    for(int i=0;i<len;i++)
+        s[i]=a[i]+b[i];
+    return s;
+}
 
 vector <vector<float>> A()
 {vector <vector<float>> d_dt={{0,0},{0,0}};
     d_dt=produitv(d, d);
    // vector <vector<float>> p(2,0);
     float  dt_y=0;
-   dt_y =produit_tv(d, y(a, b));
+   dt_y =produit_tv(d, y(x, somme(x, alphaxvect(alpha, d))));
     std::cout <<"lll"<<":"<<1/dt_y <<endl;
     for(int i=0;i<len;i++)
     {for (int j=0;j<len;j++)
@@ -93,24 +107,13 @@ vector <vector<float>> A()
         
     }
 
-vector<float> somme(vector<float> a,vector<float>b)
-{  vector<float> s(len,0);
-    for(int i=0;i<len;i++)
-        s[i]=a[i]+b[i];
-    return s;
-}
 
-vector<float> alphaxvect(int a , vector<float> m)
-{
-    for(int i=0;i<len;i++)
-        m[i]=m[i]*a;
-    return m;
-    
-}
+
+
 vector <vector<float>> B()
 {   vector <vector<float>> r={{0,0},{0,0}};
-    r= produitv(produitmv(h, y(a,b)), produitmv(h, y(a,b)));
-    float s=produit_tv(y(a,b), produitmv(h, y(a,b)));
+    r= produitv(produitmv(h, y(x, somme(x, alphaxvect(alpha, d)))), produitmv(h, y(x, somme(x, alphaxvect(alpha, d)))));
+    float s=produit_tv(y(x, somme(x, alphaxvect(alpha, d))), produitmv(h, y(x, somme(x, alphaxvect(alpha, d)))));
      std::cout <<"SSS:"<<s<<endl;
     for (int i =0;i<len;i++)
     {for (int j =0;j<len;j++)
@@ -141,8 +144,9 @@ vector <vector<float>> d_f_p()
     }
 float wolfe_rule()
 {float alphac=7,inf=0,sup=1000;
+    int iteration = 0;
     bool trouve=false;
-    while(!trouve){
+    while(!trouve&(iteration<=1000)){
 if (fonction(somme(x, alphaxvect(alphac, d)))>(fonction(x)+0.1*alphac *produit_tv(gradientf(x), d)))
     {
         sup=alphac;
@@ -160,20 +164,52 @@ else{
     return alphac;
     
 }
+float norme(vector<float> vecteur) // calcul norme d'un vecteur
+{   float n=0;
+    for(int i =0;i<len;i++)
+        n=(vecteur[i]*(vecteur[i]))+n;
+    return sqrt(n);
+}
+vector <float> quasi_newton()
+{
+    k=0;
+    vector <float> x={0,0},g =gradientf(x);
+    
+    while (norme(gradientf(x))>0.01)
+        {
+            d=alphaxvect(-1, produitmv(h, g))  ;
+             for (int i=0;i<len;i++)
+                         std::cout <<"d"<<i<<":"<<d[i] <<endl;
+            alpha = wolfe_rule();
+            std::cout <<"pas"<<k<<":"<<alpha <<endl;
+            x=somme(x,alphaxvect(-alpha, d));
+             for (int i=0;i<len;i++)
+             std::cout <<"xxx"<<i<<":"<<x[i] <<endl;
+            g=gradientf(x);
+            for (int i=0;i<len;i++)
+            std::cout <<"gggg"<<i<<":"<<g[i] <<endl;
+            h=d_f_p();
+            k=k+1;
+        }
+    
+    return x;
+    
+    
+    
+    
+    
+}
 
 
 int main(int argc, const char * argv[]) {
     // insert code here...
            
-       vector <vector<float>> matriceB={{0,0},{0,0}};
-         matriceB=d_f_p();
-       for(int i=0;i<len;i++)
-       {for (int j=0;j<len;j++)
-           std::cout <<"b:"<<i<<j<<":"<<matriceB[i][j] <<endl;}
+    vector <float> j={0,0};
+    j=quasi_newton();
+    for (int i=0;i<len;i++)
+        std::cout <<"jjj"<<":"<<j[i] <<endl;
     
-    float v=wolfe_rule();
-    std:: cout<<"v"<<v<<endl;
-    
+
     return 0;
 }
 
